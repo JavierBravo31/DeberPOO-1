8a89aaca541fafdf73249d5dc383658ace3aeefc
JavierBravo31 <javier.bravo.15.31@gmail.com>
2023-11-14 17:04:52 -0500

    first commit

---
+++ Programa 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "      , ,  " << std::endl;
+    std::cout << "     (-.-)  " << std::endl;
+    std::cout << "  O_('')('') " << std::endl;
+    std::cout << "   /     \\" << std::endl;
+    std::cout << "  /    -   \\" << std::endl;
+    std::cout << " /     _    \\" << std::endl;
+    std::cout << "(_)   (_)   (_) " << std::endl;
+
+    return 0;
+}
